597edefea5d1f2c05d5eaf7c49733dcf020aaa67
blair-developer <blairdeveloper17@gmail.com>
2024-05-26 10:32:38 +0300

    first commit

---
+++ routes/job.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+const JobController = require('../controllers/JobController');
+
+router.post('/', JobController.createJob);
+
+router.get('/', JobController.getAllJob);
+
+router.get('/search/:key', JobController.searchJobs);
+
+router.put('/:id', JobController.updateJob);
+
+router.delete('/:id', JobController.deleteJob);
+
+
+module.exports = router;
